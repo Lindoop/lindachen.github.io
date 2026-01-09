906c06a75f9bf0e62fd688a12c119e5c7732b737
Linda Chen <lindoop.chen06@gmail.com>
2026-01-09 01:01:39 -0500

    adding base

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: './',
 })
